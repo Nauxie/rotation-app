a5be3898407d617ce80d7ed0dbeddb90ad79810b
Nauxie <abhinav.tumu1@gmail.com>
2022-09-05 18:30:37 -0700

    Initial Commit

---
+++ rotation-app/rotation_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  rotation_appApp.swift
+//  rotation-app
+//
+//  Created by Abhinav  Tumu on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct rotation_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
